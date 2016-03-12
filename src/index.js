4f6826c71b6b72af292af054eef98f166bed109c
vlad <vladimir.sonnenhaft@gmail.com>
2016-03-12 17:35:00 +0100

    fixed repo for new material-ui and react versions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import 'core-js/fn/object/assign';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import AppComponent from './components/AppComponent/AppComponent';
+import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
+import AppComponent from './components/AppComponent/AppComponent';
 injectTapEventPlugin();
 
-// Render the main component into the dom
-ReactDOM.render(
-  <AppComponent />,
-  document.getElementById('app')
-);
+render(<AppComponent />, document.getElementById('app'));
